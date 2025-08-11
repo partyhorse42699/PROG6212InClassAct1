971398d93e85d9c341612dba63e28600fe35d980
partyhorse42699 <160716029+partyhorse42699@users.noreply.github.com>
2025-08-11 23:47:03 +0200

    displays some output

--- prjInventoryManagement/Program.cs
+++ prjInventoryManagement/Program.cs
@@ -13,6 +13,12 @@
                 new Product { id = 2, name = "Scissors", quantity = 2, price = 230},
                 new Product { id = 3, name = "Pens", quantity = 8, price = 8}
             };
+            Product[] products = productArray.Where(s => s.quantity < 2).ToArray();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine("Name: " + product.name + " Quantity: " + product.quantity + " Price: " + product.price);
+            }
 
         }
 
