47744d4324999562c82b197fade24dcfcf0c0c04
partyhorse42699 <160716029+partyhorse42699@users.noreply.github.com>
2025-08-11 23:37:53 +0200

    Add project files.

---
+++ prjInventoryManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace prjInventoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+            Product[] productArray =
+            {
+                new Product { id = 1, name = "Calculator", quantity = 1, price = 400 },
+                new Product { id = 2, name = "Scissors", quantity = 2, price = 230},
+                new Product { id = 3, name = "Pens", quantity = 8, price = 8}
+            };
+
+        }
+
+        public void lowStock()
+        {
+
+        }
+    }
+}
